56927cf76fec05a0c88e7aa18baec70d72f44f09
Valerie <val.conklin@gmail.com>
2017-01-03 23:22:07 -0800

    Add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Score.create([
+  {name: "airman", attempts: 6, time: 10},
+  {name: "OinkMasterPiggo", attempts: 8, time: 14},
+  {name: "weeelp", attempts: 40, time: 534},
+  {name: "minnie", attempts: 14, time: 14},
+  {name: "TinyToast", attempts: 12, time: 20},
+  {name: "HAY.LISTEN.", attempts: 10, time: 16},
+  {name: "y2k_k00l", attempts: 20, time: 18},
+  {name: "stuff", attempts: 24, time: 48},
+  {name: "tryingtho", attempts: 11, time: 105},
+  {name: "bob", attempts: 9, time: 124}
+])
